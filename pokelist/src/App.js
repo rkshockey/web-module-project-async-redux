0fa3bc530c13490edfcff3cd0cd18e9512d681e2
rkshockey <rkshockey@gmail.com>
2021-08-11 19:26:17 -0400

    Got Your Pokemon list working

--- pokelist/src/App.js
+++ pokelist/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './App.css';
-import { connect } from 'react-redux';
 
 import PokeList from './components/PokeList';
 import BuildPoke from './components/BuildPoke';
+import YourPokemon from './components/YourPokemon';
 
-function App(props) {
+function App() {
   return (
     <div className="App">
       <h1>Build a Pokemon</h1>
       <BuildPoke />
+      <YourPokemon />
       <PokeList />
     </div>
   );
